b958e369856000fd0716404ca1aaf7edb48ebb74
Andy Parker <Jandyp4@yahoo.com>
2024-01-30 15:19:10 -0600

    bug fix

--- Encoders/4X2PriorityEncoder/encoder_priority_4x2_df.v
+++ Encoders/4X2PriorityEncoder/encoder_priority_4x2_df.v
@@ -4,7 +4,7 @@ input [3:0]I;
 output [1:0]Y;
 output V; //Validation Bit
 
-assign {V,Y} = I[0]?3`b100:I[1]?3`b101:I[2]?3`b110:I[3]?3`b111:3`b000;
+assign {V,Y} = I[0]?3'b100:I[1]?3'b101:I[2]?3'b110:I[3]?3'b111:3'b000;
 
 
 endmodule

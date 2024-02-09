849ee3208c0ccc87c0eaaa9513963f394470de72
JandyP4 <43050960+JandyP4@users.noreply.github.com>
2024-02-08 21:50:25 -0600

    First Vivado Push

---
+++ Course Two (System Verilog)/Vivado Projects/Project Two/Project Two.srcs/sim_1/new/tb.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/08/2024 09:11:28 PM
+// Design Name: 
+// Module Name: tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tb(
+
+    );
+endmodule
